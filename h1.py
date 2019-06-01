aace90eb8a13a8fd9001192e71703ff19e1ae3e1
Lance Norskog <lance.norskog@walmartlabs.com>
2019-06-01 16:38:22 -0700

    finished

--- h1.py
+++ h1.py
@@ -3,7 +3,7 @@
 import sys
 
 import snlp
-import penn
+import summarize
 import cmudict
 
 cd = cmudict.CMUDict()
@@ -13,47 +13,53 @@ cd = cmudict.CMUDict()
 # Stage 1 (basic):
 #    Generate possible subtrees, including those missing certain CP tags.
 #    DONE
-# Stage 2 (entailment):
-#    Include all trees that are parent of a sample.
-#    Emit output that maps all parent trees to a sample.
-#    Allows training encoder->decoder with entailed output.
+# Stage 2 (clause summarization):
+#    Find subsentences/clauses that can be validly trimmed by dropping one or more labeled subtrees.
+#    Allows training encoder->decoder with summarized output.
 #    That is, the output is a summary of the input, instead of just a direct copy.
 #    This gives a lot more flexibility in creating a predictor.
 
 
 num_lines=0
-num_pass=0
-num_nonword=0
+num_nosyll=0
 
+nosyll_f = open("no_syll.txt", "w")
 outf_5 = open("haiku_5.txt", "w")
 outf_7 = open("haiku_7.txt", "w")
 num_5=0
 num_7=0
+last_nosyll = None
 
 drop_clauses=['adjp','whnp','s','sbar','sbarq','sinv','sq']
 
-def process(sample):
-        # print(sample)
+# accept two clauses, input generates output
+def process(input, output):
+        global num_nosyll
+        global last_nosyll
+        global num_5
+        global num_7
         sylls = 0
-        badword = False
+        nosyll = False
         # sample = snlp.punct(sample)
-        for word in sample:
-            if word in ["'", ',', '.', '``']:
+        for word in output:
+            if word in ["'", ',', '.', '`']:
                 continue
             if word in cd.stress_dict.keys():
                 sylls += len(cd.stress_dict[word])
             else:
-                num_nonword += 1
-                badword = True
-                print('bad: {}'.format(word))
+                num_nosyll += 1
+                nosyll = True
+                if word != last_nosyll:
+                    nosyll_f.write(word + '\n')
+                    last_nosyll = word
                 break
-        if badword:
-            continue
+        if nosyll:
+            return
         if sylls == 5:
-            outf_5.write(' '.join(sample) + '\n');
+            outf_5.write(summarize.emit(input) + '\t' + summarize.emit(output) + '\n');
             num_5 += 1
         elif sylls == 7:
-            outf_7.write(' '.join(sample) + '\n');
+            outf_7.write(summarize.emit(input) + '\t' + summarize.emit(output) + '\n');
             num_7 += 1
 
 
@@ -66,13 +72,19 @@ for line in sys.stdin:
     # max words can include punctuation
     # for sample in snlp.combos(t, labs=drop_clauses):
     # for sample in snlp.clauses(t, _min=1, _max=7, _minlen=10):
+    samples = {}
     for sample in snlp.clipped_unique(t, drop_clauses):
-        process(sample)
+        samples[str(sample) + '->' + str(sample)] = (sample, sample)
+    for (inp_t, outp_t) in summarize.pairs(t):
+        # print('{} -> {}'.format(inp, outp))
+        samples[str(inp_t) + '->' + str(outp_t)] = (snlp.flatten(inp_t), snlp.flatten(outp_t))
+    for (inp_t, outp_t) in samples.values():
+        process(inp_t, outp_t)
+
     num_lines += 1
 
 outf_5.close()
 outf_7.close()
 print('Found 5-sylls: {}, 7-sylls: {}'.format(num_5, num_7))
-print('Lines: {}, clauses rejected: {}'.format(num_lines, num_nonword))
+print('Lines: {}, clauses rejected for non-CMU word: {}'.format(num_lines, num_nosyll))
 snlp.stats()
-
--- summarize.py
+++ summarize.py
@@ -20,38 +20,61 @@ clause_labs = {
 
 def clauses(t):
     for st in t.subtrees():
-         if st.label() in clause_labs and len(st) > 2:
+         if st.label().upper() in clause_labs and len(st) > 1:
              yield st
 
 # Return a tree with any and all instances of X removed
+# This is the core of summarization: remove nonessential parts of the parse tree!
+# Another rewriter might be synonym substitution using PPDB.
 
 def trim_ADJP(t):
-    for st in snlp.strip(t, ['ADJP']):
+    for st in snlp.strip(t, ['ADJP', 'adjp']):
         yield st
 
 def trim_ADVP(t):
-    for st in snlp.strip(t, ['ADVP']):
+    for st in snlp.strip(t, ['ADVP', 'advp']):
         yield st
 
 def trim_WHNP(t):
-    for st in snlp.strip(t, ['WHNP']):
+    for st in snlp.strip(t, ['WHNP', 'whnp']):
         yield st
 
 rewriters = [ trim_ADJP, trim_ADVP, trim_WHNP ]
 
+# return pair of (clause, possibly trimmed version of clause)
 def pairs(t):
-    #print('tree ' + str(t))
     for clause in clauses(t):
-        #print('clause ' + str(t))
+        yield (clause, clause)
         for rewriter in rewriters:
             for st in rewriter(clause):
                 yield (clause, st)
-    
 
+def emit(words):
+    punc = {'.':None, ',':None, ';':None, ':': None, '?':None, "'s":None}
+    sent = ''
+    start = 0
+    if words[0] in punc:
+        start = 1
+    for i in range(start, len(words) - 1):
+        sent += words[i]
+        if not words[i + 1] in punc:
+            sent += ' '
+    sent += words[-1]
+    return sent
+
+def sentence(t):
+    s = str(t.flatten())
+    s = s[1:]
+    s = s[:-1]
+    words = s.split()
+    key = words[0]
+    return (key, emit(words[1:]))
+    
 if __name__ == "__main__":
     t = Tree.fromstring("(NP (NP (DT A) (NN bathroom)) (PP (IN with) (NP (NP (NNS walls)) (SBAR (WHNP (WDT that)) (S (VP (VBP are) (VP (VBN painted) (ADJP (NN baby) (JJ blue)))))))) (. .))")
 
     print('-------------------------------------------')
+    print(sentence(t))
     print('clauses, rewriters')
        
     for c in clauses(t):
@@ -68,8 +91,8 @@ if __name__ == "__main__":
     for (clause, trimmed) in pairs(t):
         #print('# {}'.format(clause))
         #print('> {}'.format(trimmed))
-        x = str(clause) + ' -> \n\t' + str(trimmed)
-        out[str(x)] = x
+        x = str(clause.flatten()) + ' -> \n\t' + str(trimmed.flatten())
+        out[x] = x
 
     for k in out.keys():
         print(k)
