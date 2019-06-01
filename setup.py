da4f77ba47e6b0426e55c6dfb5cc1075a02c0005
Lance Norskog <lance.norskog@walmartlabs.com>
2019-06-01 16:44:35 -0700

    add

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# thanks to ceddlyburge/python_world sample
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Deep Meter",
+    version="0.0.1",
+    author="Foo Bar",
+    author_email="foo@bar.com",
+    description="CMU Pronunciation Dictionary as code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LanceNorskog/deep_meter_2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
