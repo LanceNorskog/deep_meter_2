5a5eac47283d105b802acacec1fe71c6bbe5e88c
Lance Norskog <goksron@yahoo.com>
2019-07-06 13:32:56 -0700

    works, but for only one syllable->word mapping

--- cmu/mappers.py
+++ cmu/mappers.py
@@ -8,9 +8,11 @@ class Decoder:
         big_sylls = set()
 
         self.wordlist = [''] * (len(word2sylls))
+        self.wordlength = [0] * (len(word2sylls))
         for index, word in enumerate(word2sylls):
             self.wordlist[index] = word
             sylls = word2sylls[word]
+            self.wordlength[index] = len(sylls)
             for syll in sylls:
                 big_sylls.add(syll)
 
@@ -23,8 +25,8 @@ class Decoder:
 
         # indexes into wordlist
         # 5 is longest known word 
-        # idx2word[1][2] = word index means "syll #2 is first syll in word"
-        self.idx2word = np.zeros((3, len(self.idx2syll)), dtype='int32')
+        # idx2word[0][1] = word index means "syll #2 is first syll in word [index]"
+        self.idx2word = np.zeros((2, len(self.idx2syll)), dtype='int32')
         self.idx2word = self.idx2word - 1
         for index, word in enumerate(self.wordlist):
             j = 0
@@ -37,25 +39,31 @@ class Decoder:
         print('predictions: ', predictions[0])
         out = [[]] * len(predictions)
         num_sylls = len(predictions[0])
+        max_sylls = 2
         for i in range(len(predictions)):
             words = []
-            for j in range(num_sylls):
-                print('pred: ', predictions[i][j])
-                word_ind = self.idx2word[predictions[i][j]][0]
-                print(word_ind)
+            predict = predictions[i]
+            print('pred: ', predict)
+            j = 0
+            while j < num_sylls:
+                word_ind = self.idx2word[0][predict[j]]
+                print('word[{}]: {}'.format(j, word_ind))
                 if word_ind >= 0:
                     words.append(self.wordlist[word_ind])
-                    skip = 0
-                    for k in range(1, num_sylls - j):
-                        print('k: {}, pred: {}'.format( k, predictions[i][j + k]))
-                        if word_ind == self.idx2word[predictions[i][j + k]][k]:
+                    k = 1
+                    while k < max_sylls and j+k < num_sylls:
+                        print('k: {}, pred: {}'.format( k, predict[j+k]))
+                        if word_ind == self.idx2word[k][predict[j+k]]:
                             print('skip! ')
-                            skip += 1
+                            j += 1
                         else:
                             break
-                    j += skip
+                        k += 1
                 else:
-                    pass  # should break out since can't start a word
+                    print('fail')
+                    words = []
+                    break
+                j += 1
             out[i] = words
         return out
                 
@@ -75,6 +83,12 @@ if __name__ == "__main__":
     print('idx2word: ', decoder.idx2word)
     
     predict = [[decoder.syll2idx['DH AH'], decoder.syll2idx['M AH'], decoder.syll2idx['G ER'], decoder.syll2idx['IH Z'], decoder.syll2idx['HH IH R']]]
+    print(decoder.get_sentences(predict))
+    predict = [decoder.syll2idx['M AH']]]
+    print(decoder.get_sentences(predict))
+    predict = [decoder.syll2idx['G ER']]]
+    print(decoder.get_sentences(predict))
+    predict = [-1]]
     print(decoder.get_sentences(predict))
 
     # decode 5 ints into a series of words
