fc41f344dbcecefc8d21929da2b249d71e0b109e
Lance Norskog <lance.norskog@walmartlabs.com>
2019-07-05 20:17:33 -0700

    syllable decoder ring

--- cmu/mappers.py
+++ cmu/mappers.py
@@ -1,27 +1,44 @@
+import numpy as np
 
-def get_mappers(word2sylls):
-    big_sylls = set()
-    for index, word in enumerate(word2sylls):
-        sylls = word2sylls[word]
-        for syll in sylls:
-            syll = syll.replace(' ', '').lower()
-            big_sylls.add(syll)
+# no language model
 
-    syll2idx = {}
-    for index, syll in enumerate(big_sylls):
-        syll2idx[syll] = index
-    idx2syll = [0] * len(syll2idx)
-    for index, syll in enumerate(syll2idx):
-        idx2syll[index] = syll
-    
-    return (syll2idx, idx2syll)
+class Decoder:
+    def __init__(self, word2sylls):
+        self.word2sylls = word2sylls
+        big_sylls = set()
+
+        self.wordlist = [''] * len(word2sylls)
+        for index, word in enumerate(word2sylls):
+            self.wordlist[index] = word
+            sylls = word2sylls[word]
+            for syll in sylls:
+                big_sylls.add(syll)
+
+        self.syll2idx = {}
+        for index, syll in enumerate(big_sylls):
+            self.syll2idx[syll] = index
+        self.idx2syll = [0] * len(self.syll2idx)
+        for index, syll in enumerate(self.syll2idx):
+            self.idx2syll[index] = syll
+
+        # indexes into wordlist
+        # 5 is longest known word 
+        # idx2word[1][2] = word index means "syll #2 is first syll in word"
+        self.idx2word = np.zeros((5, len(self.idx2syll)), dtype='int32')
+        for index, word in enumerate(self.wordlist):
+            j = 0
+            for syll in word2sylls[word]:
+                self.idx2word[j][self.syll2idx[syll]] = index
 
 
 if __name__ == "__main__":
     syllables = {'the':['DH AH']}
-    (syll2idx, idx2syll) = get_mappers(syllables)
-    print(syll2idx.keys())
-    print(syll2idx['dhah'], idx2syll[0])
-    print('# features: ', len(idx2syll))
+    decoder = Decoder(syllables)
+    print(decoder.syll2idx.keys())
+    print(decoder.syll2idx['DH AH'], decoder.idx2syll[0])
+    print('# features: ', len(decoder.idx2syll))
 
 
+    # decode 5 ints into a series of words
+    # idx2word[0-4][index into wordlist]
+    
