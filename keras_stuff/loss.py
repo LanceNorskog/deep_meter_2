bbd25446d21a3ddde6a579327ced88992301f017
Lance Norskog <lance.norskog@walmartlabs.com>
2019-07-14 00:41:18 -0700

    fiddling

--- keras_stuff/loss.py
+++ keras_stuff/loss.py
@@ -120,8 +120,7 @@ def sparse_categorical_crossentropy_temporal(target, output, from_logits=False, 
 
     if len(output_shape) == 3:
         # if our output includes timesteps we need to reshape
-        return K.max(res, axis=-1) * scale
-        #return tf.reshape(res, tf.shape(output)[:-1])
+        return K.max(res, axis=-1) * scale + tf.reshape(res, tf.shape(output)[:-1])
     else:
         return res
 
