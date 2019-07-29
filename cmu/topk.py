06c5eb8e237d03e79d433d69249b6c63cff9eec8
Lance Norskog <lance.norskog@walmartlabs.com>
2019-07-28 17:41:05 -0700

    fiddling

--- cmu/topk.py
+++ cmu/topk.py
@@ -3,19 +3,18 @@ import numpy as np
 from mappers import Decoder
 from full import FullSearch
 
-''' return (summed weights, syllable indices) from prediction set '''
+''' return (summed weights, syllable indices) from 1 prediction set '''
 def get_top_k(predictions, top_k=5):
     print(predictions.shape)
-    assert len(predictions.shape) == 3
+    assert len(predictions.shape) == 2
 
-    vals = np.zeros((predictions.shape[0], predictions.shape[1], top_k), dtype='float32')
-    indices = np.zeros((predictions.shape[0], predictions.shape[1], top_k), dtype='int32')
-    for p in range(len(predictions)):
-        for s in range(len(predictions[0])):
-            ind = np.argsort(predictions[p][s])
-            short = np.flip(ind[-top_k:])
-            vals[p][s] = predictions[p][s][short]
-            indices[p][s] = short
+    vals = np.zeros((predictions.shape[0], top_k), dtype='float32')
+    indices = np.zeros((predictions.shape[0], top_k), dtype='int32')
+    for s in range(predictions.shape[0]):
+        ind = np.argsort(predictions[s])
+        short = np.flip(ind[-top_k:])
+        vals[s] = predictions[s][short]
+        indices[s] = short
     return (vals, indices)
 
 def decodem(scorepaths, decoder, wordset):
@@ -25,7 +24,7 @@ def decodem(scorepaths, decoder, wordset):
         #print('top_paths.shape: ', top_paths.shape)
         #print('top_paths[{}]: {}'.format(j, top_paths))
         #print('top_paths[{}][]: {}'.format(j, top_paths[0][np.arange(num_sylls), scorepaths[j]]))
-        morepaths[j] = top_paths[0][np.arange(num_sylls), scorepaths[j]]
+        morepaths[j] = top_paths[np.arange(num_sylls), scorepaths[j]]
     #print('morepaths: ' + str(morepaths))
     encoded = decoder.get_sentences(morepaths)
     sentences = {}
@@ -72,7 +71,7 @@ def short_sentences(sentences, num_sylls):
     return out
 
 if __name__ == "__main__":
-    preds = np.array([[[0, 1, 2], [4, 3, 5], [8,6,7]]])
+    preds = np.array([[0, 1, 2], [4, 3, 5], [8,6,7]])
     print('predictions: ', preds)
     (v, i) = get_top_k(preds, top_k=2)
     print('vals:  ', v)
@@ -84,7 +83,7 @@ if __name__ == "__main__":
     num_sylls = 5
     num_dict = 15
     fs = FullSearch(num_sylls * 2, num_sylls, top_k)
-    (top_vals, top_paths) = get_top_k(np.zeros((1, num_sylls, num_dict), dtype='float32'), top_k=top_k)
-    fs.mainloop(top_paths[0])
+    (top_vals, top_paths) = get_top_k(np.zeros((num_sylls, num_dict), dtype='float32'), top_k=top_k)
+    fs.mainloop(top_paths)
     sentences = decodem(fs.scorepaths, decoder, [])
     
